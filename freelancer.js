7fe761df13967f063937b2fea30eefe64b838f40
CourtneyATX <courtney.mcguire.tx@gmail.com>
2023-09-28 15:02:29 -0500

    initial freelancer and random freelancer funct

--- freelancer.js
+++ freelancer.js
@@ -1,3 +1,5 @@
+/* STATE */
+
 // at the top add the average which updates with every new freelancer
 // interval setting every 5000ms a new freelancer is added
 const addFreelancerIntervalId = setInterval(addFreelancer, 5000);
@@ -13,6 +15,38 @@ const freelancers = [
     { name: "Karen G.", occupation: "Curriculum Writer", price: 60 },
     { name: "James M.", occupation: "HVAC Designer", price: 70 },
     { name: "Scott M.", occupation: "Security Officer", price: 60 },
-]
+];
+
+// write function to render the initial freelancer
+render();
+
+function render() {
+    // Render the freelancers
+    const freelancer = document.querySelector(".freelancerName");
+    const freelancerElements = freelancers.map((freelancer) => {
+      const element = document.createElement("li");
+      element.classList.add(freelancers.name, freelancers.occupation, freelancers.price);
+      return element;
+    });
+    freelancer.replaceChildren(...freelancerElements);
+}
+// write function to generate new random freelancer
+
+function addFreelancer() {
+    const freelancer = freelancers[Math.floor(Math.random() * freelancers.length)];
+    
+    freelancers.push({name:, occupation:, price:});
+  
+    render();
+  
+        if(freelancers.length === freelancers.length){
+          clearInterval(addFreelancerIntervalId)
+        }
+  }
+
+
+// write function to calculate average price
 
 // 
+
+
